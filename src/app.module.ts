04b33a0208da78083215f7cf85ede14996b51456
ramisseker <ramis.seker@gmail.com>
2023-07-26 15:31:57 +0300

    feat: create product controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/store'), ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Put,
+  Delete,
+  Body,
+  Param,
+  Query,
+  NotFoundException,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDTO } from './dtos/create-product.dto';
+import { FilterProductDTO } from './dtos/filter-product.dto';
+@Controller('store/product')
+export class ProductController {
+  constructor(private productService: ProductService) {}
 
-@Controller('product')
-export class ProductController {}
+  @Get('/')
+  async getProducts(@Query() filterProductDTO: FilterProductDTO) {
+    if (Object.keys(filterProductDTO).length) {
+      const filteredProducts = await this.productService.getFilteredProducts(
+        filterProductDTO,
+      );
+
+      return filteredProducts;
+    } else {
+      const allProducts = await this.productService.getAllProducts();
+
+      return allProducts;
+    }
+  }
+
+  @Post('/')
+  async addProduct(@Body() createProductDTO: CreateProductDTO) {
+    const product = await this.productService.addProduct(createProductDTO);
+
+    return product;
+  }
+
+  @Put('/:id')
+  async updateProduct(
+    @Param('id') id: string,
+    @Body() createProductDTO: CreateProductDTO,
+  ) {
+    const product = await this.productService.updateProduct(
+      id,
+      createProductDTO,
+    );
+    if (!product) throw new NotFoundException('Product does not exist!');
+
+    return product;
+  }
+
+  @Delete('/:id')
+  async deleteProduct(@Param('id') id: string) {
+    const product = await this.productService.deleteProduct(id);
+    if (!product) throw new NotFoundException('Product does not exist!');
+
+    return product;
+  }
+}
